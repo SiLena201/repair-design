ccb0c0f773d6fa070ae670050207f3402ba36928
nova-quick <alena.simushina1999@gmail.com>
2020-03-13 23:37:04 +0700

    feat: js function for modal

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(event){ 
+ const modal = document.querySelector('.modal');
+ const modalBtn = document.querySelectorAll('[data-toggle=modal');
+ const closeBtn = document.querySelector('.modal__close');
+ const switchNodal = () => {
+  modal.classList.toggle('modal--visible');
+ }
+
+ modalBtn.forEach(element => {
+  element.addEventListener('click', switchNodal)
+ });
+
+ closeBtn.addEventListener('click', switchNodal)
+ 
+});
