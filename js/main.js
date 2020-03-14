624259c8da73af61a3ee884e10822d2478ea5828
nova-quick <alena.simushina1999@gmail.com>
2020-03-14 15:13:21 +0700

    choe:.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function(event){ 
+document.addEventListener("DOMContentLoaded", function(event) { 
  const modal = document.querySelector('.modal');
  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
  const closeBtn = document.querySelector('.modal__close');
- const switchNodal = () => {
+ const switchModal = () => {
   modal.classList.toggle('modal--visible');
  }
-
+ 
  modalBtn.forEach(element => {
-  element.addEventListener('click', switchNodal);
+  element.addEventListener('click', switchModal);
  });
 
- closeBtn.addEventListener('click', switchNodal);
+  closeBtn.addEventListener('click', switchModal);
  
 });
